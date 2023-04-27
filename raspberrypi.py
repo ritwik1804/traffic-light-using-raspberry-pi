0fb7a4410c890fe8bede7879871a9eea84bf1395
Ritwik Mohanty <103522580+ritwik1804@users.noreply.github.com>
2023-04-27 11:44:52 +0530

    Create raspberrypi.py

---
+++ raspberrypi.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led_pin_1 = 4
+led_pin_2 = 15
+led_pin_3 = 18
+GPIO.setup(led_pin_1, GPIO.OUT)
+GPIO.setup(led_pin_2, GPIO.OUT)
+GPIO.setup(led_pin_3, GPIO.OUT)
+
+#for i in range (1000):
+for i in range(1):
+    GPIO.output(led_pin_1, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(led_pin_1, GPIO.LOW)
+    time.sleep(0.5)
+
+
+for i in range(1):
+    GPIO.output(led_pin_2, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(led_pin_2, GPIO.LOW)
+    time.sleep(0.5)
+    
+for i in range(1):
+    GPIO.output(led_pin_3, GPIO.HIGH)
+    time.sleep(10)
+    GPIO.output(led_pin_3, GPIO.LOW)
+    time.sleep(0.5)
+
+GPIO.cleanup()
